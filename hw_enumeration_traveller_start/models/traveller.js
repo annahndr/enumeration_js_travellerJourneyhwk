7547f267f4f30b3ccbcc1167154c46e99db9ee86
Anna Henderson <annahenderson@gmail.com>
2018-10-10 21:21:14 +0100

    MVP and extensions

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -3,32 +3,51 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
 
 };
 
 Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map((journey) => {
+    return journey.transport;
+  })
 
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    if(journey.transport === transport){
+      return journey;
+    }
+  })
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) =>{
+    if(journey.distance > minDistance){
+      return journey;
+    }
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((accumulator, journey) => {
+    return accumulator + journey.distance
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
+  let unique = [...new Set(this.journeys.map(journey => journey.transport))]
+    return unique;
 };
-
 
 module.exports = Traveller;
